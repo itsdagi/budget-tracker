f5988fcbd31833914487781b06438ee69eecbcb6
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-06 10:18:01 +0000

    commit

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const form = document.querySelector(".add");
 let transactions = localStorage.getItem("transactions") !== null ?JSON.parse(localStorage.getItem("transactions")): [];
 
-form.addEventListener("submit", event=>{
-  event.preventDefault()
+
+
+function addTransaction(){
   const time = new Date();
   const transaction = {
     id: Math.floor(math.random()*100000),
-    source: form.source.value,
-    amount: form.amount.value,
+    source: source,
+    amount: amount,
     time: `${time.toLocaleTimeString()} ${time.toLocaleDateString()}`
   };
   transactions.push(transaction);
   localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+form.addEventListener("submit", event=>{
+  event.preventDefault()
+  addTransaction(form.source.value, form.amount.value)
 })
